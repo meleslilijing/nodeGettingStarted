9c752c758193fa43dcba215d59a84b22291d4942
lilijing <lilijing@xiaomi.com>
2015-10-12 11:22:03 +0800

    change files structor

---
+++ nodejs教程/stream/writeStream.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var data = "It's a writeStream demo on nodejs."
+
+var writeStream = fs.createWriteStream('output.txt');
+
+writeStream.write(data, 'UTF8');
+
+writeStream.end();
+
+writeStream.on('finish', function() {
+	console.log("写入完成");
+})
+
+writeStream.on('error', function() {
+	console.log(err.stack);
+})
